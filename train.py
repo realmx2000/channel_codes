733bd489f3e35c96e7fdabf2701e13b91ad410fe
Michael <mlko53@stanford.edu>
2019-02-23 02:33:17 -0800

    organize train.py and directory

--- train.py
+++ train.py
@@ -3,22 +3,32 @@ from dataset import InputDataloader
 from args import ArgParser
 from models import Encoder
 
-parser = ArgParser()
-args = parser.parse_args()
-num_examples = args.num_epochs * args.batches_per_epoch * args.batch_size
-loader = InputDataloader(args.batch_size, args.block_length, num_examples, True)
 
+def train(args):
+    """Train the model on the dataset."""
+    write_args(args)
 
-it = loader.get_loader()
-next = it.get_next()
-sess = tf.Session()
-encoder = Encoder(100, 25, 2, 1/2, False)
-sess.run(tf.global_variables_initializer())
-sess.run(tf.local_variables_initializer())
-for i in range(100):
-    val = sess.run(encoder.forward(next))
-    print(val)
-    input()
+    model_args = args.model_args
+    data_args = args.data_args
+    logger_args = args.logger_args
+
+    num_examples = data_args.num_epochs * data_args.batches_per_epoch * data_args.batch_size
+	loader = InputDataloader(data_args.batch_size, data_args.block_length, num_examples, True)
+
+	it = loader.get_loader()
+	next = it.get_next()
+	sess = tf.Session()
+	encoder = Encoder(100, 25, 2, 1/2, False)
+	sess.run(tf.global_variables_initializer())
+	sess.run(tf.local_variables_initializer())
+	for i in range(100):
+	    val = sess.run(encoder.forward(next))
+	    print(val)
+	    input()
+
+if __name__ == '__main__':
+    parser = TrainArgParser()
+    train(parser.parse_args())
 
 """
 SNR = 1/5
