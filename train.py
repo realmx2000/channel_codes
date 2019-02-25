2490f7c7be6ec721014e9527036e3dd04dc29237
Joe Lou <realmx2000@DN0a1fa6d8.SUNet>
2019-02-25 12:18:23 -0800

    Delete old code

--- train.py
+++ train.py
@@ -22,28 +22,7 @@ def write_args(args):
                         args_dict[k][k2] = v2.as_posix()
         json.dump(args_dict, fh, indent=4, sort_keys=True)
         fh.write('\n')
-
-"""
-def train(args):
-    write_args(args)
-
-    model_args = args.model_args
-    data_args = args.data_args
-    logger_args = args.logger_args
-
-    num_examples = data_args.num_epochs * data_args.batches_per_epoch * data_args.batch_size
-    loader = InputDataloader(data_args.batch_size, data_args.block_length, num_examples, True)
-
-    generator = loader.example_generator()
-    sess = tf.Session()
-    encoder = Encoder(100, 25, 2, 1/2, False)
-    sess.run(tf.global_variables_initializer())
-    sess.run(tf.local_variables_initializer())
-    for i in range(100):
-        val = sess.run(encoder.forward(next))
-        print(val)
-        input()
-"""
+        
 
 def train(args):
 
